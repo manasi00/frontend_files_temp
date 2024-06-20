a41d95ced3102ca338b91233fe056b86a6190346
manasi <maanasee341@gmail.com>
2024-06-20 15:53:53 +0545

    added all files

---
+++ Desktop/projectsANDpdfs/dashguardian360/orders.js
@@ -0,0 +1,20 @@
+const Recents = [
+    {
+        MemberName: 'Manasi',
+        Entry: '5:00 AM',
+        Exit: '8:00 AM',
+        status: 'OK'
+    },
+    {
+       MemberName: 'Shristy',
+        Entry: '7:00 AM',
+        Exit: '8:00 AM',
+        status: 'OK'
+    },
+    {
+        MemberName: 'Bhawana',
+        Entry: '10:00 AM',
+        Exit: '2:00 PM',
+        status: 'OK'
+    },
+]
